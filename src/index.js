20fec7bd05edf102646e0522567bd9d78fdd2fb9
Grace Chen <gychen2@illinois.edu>
2020-12-21 14:44:57 -0800

    Updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-
 import './index.css';
+//import reportWebVitals from './reportWebVitals';
+
+import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
